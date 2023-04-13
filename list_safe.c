36b63b657dbb67661bf2cabcf8b228b2ed06c5ce
Prudius Yurii <yurii.prudius@geeksforless.com>
2023-04-13 13:52:00 +0300

    basic

---
+++ list_safe.c
@@ -0,0 +1,161 @@
+#include "list_safe.h"
+
+/**
+ * Create list
+ *
+ * @return list_safe_t*
+ */
+list_safe_t *list_safe_construct()
+{
+    list_safe_t *list = calloc(1, sizeof(list_safe_t));
+    list_safe_init(list);
+
+    return list;
+}
+
+/**
+ * Init empty list
+ *
+ * @param list
+ */
+void list_safe_init(list_safe_t *list)
+{
+    if (list)
+    {
+        list->head = NULL;
+        list->last = NULL;
+        pthread_mutex_init(&list->mutex, NULL);
+        pthread_cond_init(&list->cond, NULL);
+    }
+}
+
+/**
+ * Clear list & destruct values
+ *
+ * @param list
+ * @param destructor
+ */
+void list_safe_clear(list_safe_t *list, void (*destructor)(void *))
+{
+    if (list)
+    {
+        pthread_mutex_lock(&list->mutex);
+
+        list->last = NULL;
+        while (list->head != NULL)
+        {
+            record_safe_t *node = list->head;
+            if (destructor)
+            {
+                destructor(node->record);
+            }
+            list->head = node->next;
+            free(node);
+        }
+
+        pthread_mutex_unlock(&list->mutex);
+    }
+}
+
+/**
+ * Clear & destruct list
+ *
+ * @param list
+ * @param destructor
+ * @return list_t*
+ */
+list_t *lists_safe_destruct(list_safe_t *list, void (*destructor)(void *))
+{
+    if (list)
+    {
+        list_safe_clear(list, destructor);
+        pthread_mutex_destroy(&list->mutex);
+        pthread_cond_destroy(&list->cond);
+        free(list);
+    }
+
+    return NULL;
+}
+
+/**
+ * Add to end of queue
+ *
+ * @param list
+ * @param value
+ */
+void list_safe_add(list_safe_t *list, void *value)
+{
+    if (list)
+    {
+        record_safe_t *new_node = malloc(sizeof(record_safe_t));
+        new_node->next = NULL;
+        new_node->record = value;
+
+        pthread_mutex_lock(&list->mutex);
+
+        if (list->last)
+        {
+            list->last->next = new_node;
+        }
+        else
+        {
+            list->head = new_node;
+        }
+        list->last = new_node;
+
+        pthread_cond_signal(&list->cond);
+        pthread_mutex_unlock(&list->mutex);
+    }
+}
+
+/**
+ * Add to head of stack
+ *
+ * @param list
+ * @param value
+ */
+void list_safe_push(list_safe_t *list, void *value)
+{
+    if (list)
+    {
+        record_safe_t *new_node = malloc(sizeof(record_safe_t));
+        new_node->next = list->head;
+        new_node->value = value;
+
+        pthread_mutex_lock(&list->mutex);
+
+        list->head = new_node;
+        if (!list->last)
+        {
+            list->last = new_node;
+        }
+
+        pthread_cond_signal(&list->cond);
+        pthread_mutex_unlock(&list->mutex);
+    }
+}
+
+/**
+ * Remove from queue head
+ *
+ * @param list
+ * @return void*
+ */
+void *list_safe_remove(list_safe_t *list)
+{
+    pthread_mutex_lock(&list->mutex);
+
+    while (list->head == NULL)
+    {
+        pthread_cond_wait(&list->cond, &list->mutex);
+    }
+
+    record_safe_t *node = list->head;
+    void *value = node->record;
+    list->head = node->next;
+    free(node);
+
+    pthread_mutex_unlock(&list->mutex);
+
+    return value;
+}
---
+++ list_safe.h
@@ -0,0 +1,36 @@
+#ifndef LIST_SAFE_H
+#define LIST_SAFE_H
+
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct record_safe
+{
+    struct record_safe *next;
+    void *value;
+} record_safe_t;
+
+typedef struct list_safe
+{
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+
+    record_safe_t *head;
+    record_safe_t *last;
+} list_safe_t;
+
+list_safe_t *list_safe_construct();
+void list_safe_init(list_safe_t *list);
+void list_safe_clear(list_safe_t *list, void (*destructor)(void *));
+list_t *lists_safe_destruct(list_safe_t *list, void (*destructor)(void *));
+
+void list_safe_add(list_safe_t *list, void *value); // to end
+void list_safe_push(list_safe_t *list, void *value); // to head 
+void *list_safe_remove(list_safe_t *list);
+#define list_safe_pop(list) list_safe_remove(list);
+
+#define list_safe_foreach(list) for (record_safe_t *node = list->head; node; node = node->next)
+
+#endif // LIST_SAFE_H
