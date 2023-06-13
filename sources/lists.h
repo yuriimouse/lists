83822b6fb55d3e1afee9b8dba698786fe48f8e4c
Iurii Prudius <iprudius@geeksforless.com>
2023-06-13 14:47:11 +0300

    list_foreach

--- sources/lists.h
+++ sources/lists.h
@@ -22,12 +22,12 @@ void list_init(list_t *list);
 void list_clear(list_t *list, void (*destructor)(void *));
 list_t *list_destruct(list_t *list, void (*destructor)(void *));
 
-void list_add(list_t *list, void *value); // to end
-void list_push(list_t *list, void *value); // to head 
+void list_add(list_t *list, void *value);  // to end
+void list_push(list_t *list, void *value); // to head
 void *list_remove(list_t *list);
 #define list_pop(list) list_remove(list)
 void *list_get(list_t *list);
 
-#define list_foreach(list) for (record_t *node = list->head; node; node = node->next)
+#define list_foreach(list) for (record_t *node = list ? list->head : NULL; node; node = node->next)
 
 #endif // LISTS_H
