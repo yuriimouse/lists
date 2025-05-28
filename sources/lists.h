aaee834f3ff5da1c2f3549a460e3d0922fba2a6f
Yurii <12498611+yuriimouse@users.noreply.github.com>
2025-05-28 12:18:48 +0300

    Update lists.h
    Versioning

--- sources/lists.h
+++ sources/lists.h
@@ -1,9 +1,17 @@
-#ifndef LISTS_H
+fine#ifndef LISTS_H
 #define LISTS_H
 
 #define _GNU_SOURCE
 
 #include <stdlib.h>
+
+// versioning
+#define STR_INDIR(x) #x
+#define STR(x) STR_INDIR(x)
+#define VERSION_LIST_MAJOR 2
+#define VERSION_LIST_MINOR 1
+#define VERSION_LIST_PATCH 1
+#define VERSION_LIST STR(VERSION_LIST_MAJOR) "." STR(VERSION_LIST_MINOR) "." STR(VERSION_LIST_PATCH)
 
 typedef struct record
 {
