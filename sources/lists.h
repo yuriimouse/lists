6318933d348a9d178f0a77ff937214ad76e99b65
Yurii <12498611+yuriimouse@users.noreply.github.com>
2025-05-29 10:01:42 +0300

    Update lists.h

--- sources/lists.h
+++ sources/lists.h
@@ -21,15 +21,15 @@ typedef struct list
 } list_t;
 
 list_t *list_construct(void);
-void list_init(list_t *list);
-void list_clear(list_t *list, void (*destructor)(void *));
+list_t *list_clear(list_t *list, void (*destructor)(void *));
 list_t *list_destruct(list_t *list, void (*destructor)(void *));
 
-void list_add(list_t *list, void *value);  // to end
-void list_push(list_t *list, void *value); // to head
+void *list_add(list_t *list, void *value);  // to end
+void *list_push(list_t *list, void *value); // to head
 void *list_remove(list_t *list);
 #define list_pop(list) list_remove(list)
 void *list_get(list_t *list);
+#define list_peek(list) list_get(list)
 
 #define list_foreach(list) for (record_t *node = list ? list->head : NULL; node; node = node->next)
 
