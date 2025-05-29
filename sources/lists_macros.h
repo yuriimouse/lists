125c0bc81e5c8913ff620213e39b3cab0ce6bd66
Yurii <12498611+yuriimouse@users.noreply.github.com>
2025-05-29 08:27:18 +0300

    Create lists_macros.h

---
+++ sources/lists_macros.h
@@ -0,0 +1,15 @@
+#ifndef LISTS_MACROS_H
+#define LISTS_MACROS_H
+
+// versioning
+#define STR_INDIR(x) #x
+#define STR(x) STR_INDIR(x)
+#define VERSION_LIST_MAJOR 2
+#define VERSION_LIST_MINOR 2
+#define VERSION_LIST_PATCH 1
+#define VERSION_LIST STR(VERSION_LIST_MAJOR) "." STR(VERSION_LIST_MINOR) "." STR(VERSION_LIST_PATCH)
+
+#define CHECK_ALLOC(ptr) \
+    if (!(ptr))
+
+#enfif // LISTS_MACROS_H
